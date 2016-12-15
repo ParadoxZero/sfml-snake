a8bed322411d9709d5c8be0ae7fb212ca5446196
ParaodxZero <sidhin.thomas@gmail.com>
2016-12-15 06:28:23 +0530

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Snake", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Snake", sf::Style::Close);
 	gameSpace::Game gameController(&window);
 	gameController.start();
 	return 0;
