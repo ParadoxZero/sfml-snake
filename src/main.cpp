365df686812a90c074e0c0d1435bd5f67b7e2cf5
Sidhin S Thomas <thomas.sidhin@outlook.com>
2024-04-06 13:10:00 +0530

    Fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 /*
-*	Copyright (C) 2016 Sidhin S Thomas
-*
-*	This file is part of sfml-snake.
-*
-*    sfml-snake is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   sfml-snake is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with sfml-snake.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *	Copyright (C) 2016 Sidhin S Thomas
+ *
+ *	This file is part of sfml-snake.
+ *
+ *    sfml-snake is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   sfml-snake is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with sfml-snake.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "ui/MainMenu.h"
- 
+
 int main() {
-	sf::RenderWindow window( sf::VideoMode( 800, 800 ), "Snake", sf::Style::Close );
-	game::MainMenu menu( window );
-	menu.start();
-	return 0;
+  sf::RenderWindow window(sf::VideoMode(800, 800), "Snake", sf::Style::Close);
+  game::MainMenu menu(window);
+  menu.start();
+  return 0;
 }
